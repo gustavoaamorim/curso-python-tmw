f55377dc2880610022ac260be97c38d1c6bf524d
gustavoaamorim <amorim.gustavo@outlook.com>
2024-02-12 15:03:10 -0300

    update exercicios e arquivos dia 4

---
+++ dia04/loteria.py
@@ -0,0 +1,28 @@
+def entrada_usuario(msg):
+    """
+    Validação de entrada do usuário para garantir que é um número e não uma string
+    """
+    while True:
+
+        try:
+            numero = int(input(msg))
+            return numero
+        except ValueError as err:
+            print('Entre com um valor válido!\n')
+
+sorte = 7
+
+# definindo um range com 3 tentativas de acerto
+for i in range(3):
+    numero = entrada_usuario('Tente um valor entre 1 e 15: ')
+
+    if numero < sorte:
+        print('Tente um número maior!')
+    elif numero > sorte:
+        print('Tente um número menor!')
+    else:
+        print('Acertou miserávi!')
+        break
+
+else:
+    print('Acabaram suas tentativas!')
